a349bbff6880071167964ce9e7d15227d171a8a1
Ralf Matulat <ralf.matulat@bundestag.de>
2015-09-04 09:49:20 +0200

    nacl.fileutils with more unittests
    Added meaningful unittests, remove weird unittests

--- nacl/fileutils.py
+++ nacl/fileutils.py
@@ -4,8 +4,9 @@
 """ Contains code for file operations in a very broad sense of
     it's meaning."""
 import salt.config
-# from pprint import pprint
+from pprint import pprint
 from distutils import spawn
+from nacl.decorator import log
 import fnmatch
 import os
 import json
@@ -36,7 +37,6 @@ def get_salt_root_dirs():
         dir_list.extend(values)
 
     return sorted(set(dir_list))
-    # return ['/srv/salt/dev']
 
 
 def get_dir_list_from_filesystem(filter=None):
@@ -47,6 +47,7 @@ def get_dir_list_from_filesystem(filter=None):
         return matches
 
     starter = get_salt_root_dirs()
+
     for start in starter:
         for root, dirnames, filenames in os.walk(start):
             if fnmatch.fnmatch(root, filter):
@@ -55,16 +56,18 @@ def get_dir_list_from_filesystem(filter=None):
     return matches
 
 
+@log
 def get_users_nacl_conf():
     """ returns the users nacl configuration """
 
     user_home = os.path.expanduser("~")
     user_config = {}
+    _ret = []
 
     try:
         with open(user_home + '/.nacl') as data_file:
             user_config = json.load(data_file)
-        return user_config
+        return {'direct_out': user_config}
     except:
-        print("[ERROR] ~/.nacl not found or invalid JSON")
-        sys.exit(3)
+        _ret.append(("FAIL", " ~/.nacl not found or invalid JSON", 3))
+        return _ret
--- test/test_nacl_fileutils.py
+++ test/test_nacl_fileutils.py
@@ -10,23 +10,62 @@ from nacl.fileutils import get_users_nacl_conf
 
 class TestNaclFileUtils(unittest.TestCase):
 
+    # binary_exists()
+
     def test_binary_exists(self):
         self.assertTrue(binary_exists('ls'))
         self.assertFalse(binary_exists('not_existing'))
 
-    def test_get_salt_root_dirs(self):
-        self.assertIsInstance(get_salt_root_dirs(), list)
+    # get_salt_root_dirs()
+
+    sample_salt_config = {
+        'pillar_roots': {
+            'base': ['/srv/pillar/base'],
+            'dev': ['/srv/pillar/dev'],
+            'prod': ['/srv/pillar/prod']},
+        'file_roots': {
+            'base': ['/srv/salt/base'],
+            'prod': ['/srv/salt/prod'
+                     '/srv/formulas/nginx-formula']}}
+
+    @mock.patch('salt.config.client_config', return_value=sample_salt_config)
+    def test_get_salt_root_dir(self, mock_config):
+        self.assertEqual(['/srv/pillar/base',
+                          '/srv/pillar/dev',
+                          '/srv/pillar/prod',
+                          '/srv/salt/base',
+                          '/srv/salt/prod/srv/formulas/nginx-formula'],
+                         get_salt_root_dirs())
+
+    # get_dir_list_from_filesystem()
+
+    @mock.patch('nacl.fileutils.get_salt_root_dirs', return_value=['/bar'])
+    @mock.patch('os.walk',
+                return_value=[('/fooo/.git', 'dirname', 'filename')])
+    def test_get_dir_list_from_filesystem_foo_git(self,
+                                                  mock_gsrd,
+                                                  mock_oswalk):
+        self.assertEqual(['/fooo/.git'], get_dir_list_from_filesystem('*.git'))
 
     def test_get_dir_list_from_filesystem(self):
         """ Only checks whether a list is returned """
-        self.assertIsInstance(get_dir_list_from_filesystem(), list)
+        self.assertEqual([], get_dir_list_from_filesystem())
 
-    # Mocking stuff away
+    # get_users_nacl_conf()
+
     @mock.patch('os.path.expanduser', return_value="/tmp/")
     @mock.patch('__builtin__.open')
-    @mock.patch('json.load', return_value="{ 'foo': 'bar' }")
+    @mock.patch('json.load', return_value="{'foo': 'bar'}")
     def test_get_users_nacl_conf(self, os_mock, open_mock, json_mock):
-        self.assertTrue(get_users_nacl_conf())
+        self.assertEqual("{'foo': 'bar'}", get_users_nacl_conf())
+
+    # test the except block
+    @mock.patch('os.path.expanduser', return_value="/tmp/")
+    @mock.patch('__builtin__.open')
+    @mock.patch('json.load', side_effect=TypeError())
+    def test_get_users_nacl_conf_raises(self, os_mock, open_mock, json_mock):
+        self.assertEqual([('FAIL', ' ~/.nacl not found or invalid JSON', 3)],
+                         get_users_nacl_conf._fn())
 
 if __name__ == '__main__':
     unittest.main()
